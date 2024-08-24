c2e8a9efbc49f226573e5908619c33aa22a79364
Anish Singla <singlaanish56@gmail.com>
2024-08-24 05:59:34 +0530

    AniRij V1

---
+++ keyboards/handwired/anirij/anirij_v1/keymaps/vial/config.h
@@ -0,0 +1,14 @@
+
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE4, 0xC8, 0x75, 0x1B, 0x3F, 0x1A, 0x43, 0x8A}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1}
+
+#define ENCODERS_PAD_A { D2 , B3}
+#define ENCODERS_PAD_B { D3 , B2}
+#define ENCODER_RESOLUTION 4
+
+// #define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+// #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
